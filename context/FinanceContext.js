2807f59c899a9e61c44fb3d3e0d90bfeac8bd33a
magarharshal452-lang <magarharshal452@gmail.com>
2026-01-21 23:09:58 +0530

    Update FinanceContext.js

--- context/FinanceContext.js
+++ context/FinanceContext.js
@@ -1,1 +1,24 @@
+import React, { createContext, useState } from "react";
+
+export const FinanceContext = createContext();
+
+export const FinanceProvider = ({ children }) => {
+  const [userName, setUserName] = useState("");
+  const [accounts, setAccounts] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  const [budgets, setBudgets] = useState([]);
+
+  return (
+    <FinanceContext.Provider
+      value={{
+        userName, setUserName,
+        accounts, setAccounts,
+        transactions, setTransactions,
+        budgets, setBudgets
+      }}
+    >
+      {children}
+    </FinanceContext.Provider>
+  );
+};
 
