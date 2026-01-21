e11260d7431d4430457a141e5e7b98ef835b9b04
magarharshal452-lang <magarharshal452@gmail.com>
2026-01-21 23:03:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,43 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { FinanceProvider } from "./context/FinanceContext";
+
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import AccountsScreen from "./screens/AccountsScreen";
+import TransactionsScreen from "./screens/TransactionsScreen";
+import AddTransactionScreen from "./screens/AddTransactionScreen";
+import BudgetsScreen from "./screens/BudgetsScreen";
+import InsightsScreen from "./screens/InsightsScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => (
+  <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="Accounts" component={AccountsScreen} />
+    <Tab.Screen name="Transactions" component={TransactionsScreen} />
+    <Tab.Screen name="Budgets" component={BudgetsScreen} />
+    <Tab.Screen name="Insights" component={InsightsScreen} />
+    <Tab.Screen name="Settings" component={SettingsScreen} />
+  </Tab.Navigator>
+);
+
+export default function App() {
+  return (
+    <FinanceProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Tabs" component={Tabs} />
+          <Stack.Screen name="AddTransaction" component={AddTransactionScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </FinanceProvider>
+  );
+}
 
